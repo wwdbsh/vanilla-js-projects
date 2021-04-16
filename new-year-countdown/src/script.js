ec1e95be9e24d7dd95ba9a651c3ebfb02f6a1a9a
Sangheon Lee <wwdbsh@gmail.com>
2021-04-16 14:09:28 -0400

    20210416:new-year-countdown js

--- new-year-countdown/src/script.js
+++ new-year-countdown/src/script.js
@@ -18,5 +18,30 @@ export const getDocumentElements = () => {
     countdown = document.getElementById("countdown");
     year = document.getElementById("year");
     loading = document.getElementById("loading");
-    year.innerText = currentYear + 1;
+    year.innerText = currentYear + 1; // set background year
 };
+
+const updateCountdown = () => {
+    const currentTime = new Date();
+    const diff = newYearTime - currentTime;
+
+    const d = Math.floor(diff/1000/60/60/24);
+    const h = Math.floor(diff/1000/60/60)%24;
+    const m = Math.floor(diff/1000/60)%60;
+    const s = Math.floor(diff/1000)%60;
+
+    // add values to DOM
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+};
+
+// show spinner before countdown
+setTimeout(() => {
+    loading.remove();
+    countdown.style.display = "flex";
+}, 1000);
+
+// run every second
+setInterval(updateCountdown, 1000);
