ea8af79651c6e2c20faf19e9d08bf0d55aedf6ce
Sangheon Lee <wwdbsh@gmail.com>
2021-04-20 17:09:10 -0400

    20210420:my-momentum name-modal js localstorage for register user

--- projects/my-momentum/src/script.js
+++ projects/my-momentum/src/script.js
@@ -5,14 +5,54 @@ let body = null; // body
 let clock = null; // clock
 let greet = null; // greeting part
 
+// name modal
+let nameModalContainer = null;
+let nameModalInput = null;
+let nameModalBtn = null;
+
 export const runScript = () => {
     body = document.getElementsByTagName("body")[0];
     clock = document.getElementById("clock");
     greet = document.getElementById("greet");
-
-    // greet.innerText = GREETING_EXPR[0] + "Sangheon";
+    nameModalContainer = document.getElementById("name-modal-container");
+    nameModalInput = document.getElementById("name-input");
+    nameModalBtn = document.getElementById("name-submit-btn");
+    
+    addEventListeners(); // add events
+    checkRegisteredUser();
     generateRandomBgImage();
     updateCurrentTime();
+};
+
+// add events
+const addEventListeners = () => {
+    // name modal events
+    nameModalBtn.addEventListener("click", registerUser); // name modal btn
+    nameModalInput.addEventListener("keyup", e => {if(e.keyCode === 13) registerUser();});
+    
+    
+};
+
+// check if a user is registered
+const checkRegisteredUser = () => {
+    const user = localStorage.getItem("reg-user");
+    if(user){
+        nameModalContainer.classList.remove("show-modal");
+        greet.innerText = GREETING_EXPR[0] + user;
+    }else{
+        nameModalContainer.classList.add("show-modal");
+    }
+};
+
+
+// register an user
+const registerUser = () => {
+    const name = nameModalInput.value;
+    if(name){
+        localStorage.setItem("reg-user", name);
+        greet.innerText = GREETING_EXPR[0] + name;
+        nameModalContainer.classList.remove("show-modal");
+    }
 };
 
 // get a random background image
