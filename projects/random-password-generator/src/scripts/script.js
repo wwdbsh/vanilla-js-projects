8431a710a986fbde600548f7bbfe4e82dd16ac73
Sangheon Lee <wwdbsh@gmail.com>
2021-08-07 13:31:02 -0700

    20210807:random-password-generator script

--- projects/random-password-generator/src/scripts/script.js
+++ projects/random-password-generator/src/scripts/script.js
@@ -1,14 +1,42 @@
-let upperBtn = null;
+const settingBtns = []; // 0: upper, 1: lower, 2: numbers, 3: symbols
+let rangeCtrl = null;
+let lenDisplay = null;
 
 export const runScript = () => {
-    upperBtn = document.getElementById("upper-btn");
-    upperBtn.addEventListener("click", e => {
-        if(e.target.parentNode.classList.contains("active")){
-            e.target.parentNode.classList.remove("active");
-            e.target.classList.remove("active");
-        }else{
-            e.target.parentNode.classList.add("active");
-            e.target.classList.add("active");
-        }
+    init();
+};
+
+const init = () => {
+    [   document.getElementById("upper-btn"),
+        document.getElementById("lower-btn"),
+        document.getElementById("number-btn"),
+        document.getElementById("symbol-btn"),
+    ].forEach(btn => settingBtns.push(btn));
+    rangeCtrl = document.getElementById("range-controller");
+    lenDisplay = document.getElementById("length-value");
+    addEventListeners();
+};
+
+const addEventListeners = () => {
+    settingBtns.forEach(btn => {
+        btn.addEventListener("click", e => {
+            if(btn.classList.contains("active")){
+                btn.childNodes[0].classList.remove("active");
+                btn.classList.remove("active");
+            }else{
+                btn.childNodes[0].classList.add("active");
+                btn.classList.add("active");
+            }
+            switch(btn.id){
+                case "upper-btn":
+                    break;
+                case "lower-btn":
+                    break;
+                case "number-btn":
+                    break;
+                case "symbol-btn":
+            }
+        });
     });
+    rangeCtrl.addEventListener("change", () => lenDisplay.innerText = rangeCtrl.value);
 };
